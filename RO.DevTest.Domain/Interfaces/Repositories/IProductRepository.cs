7ae7458fef281b4287ec6453ae6ea2c3ec24ccb6
KEWEN SILVA <kewensilva58@gmail.com>
2025-04-23 20:40:00 -0300

    :hammer: [Feat: add repository interface product]

---
+++ RO.DevTest.Domain/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,10 @@
+using RO.DevTest.Domain.Entities;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<Product?> GetByIdAsync(Guid id);
+    Task AddAsync(Product product);
+    Task UpdateAsync(Product product);
+    Task DeleteAsync(Guid id);
+}
